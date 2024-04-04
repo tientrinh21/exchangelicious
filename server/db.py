92d215eabc985e5c39b54e8928d17d668518571a
Lise B. Ullevålseter <liseuliseu1@gmail.com>
2024-04-04 15:12:28 +0900

    #3 we connected to and quired the database

--- server/db.py
+++ server/db.py
@@ -1,5 +1,31 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import URL, create_engine, text
 
-db_conn_str = "mysql+pymysql://exchange:Unipovex8@server8-sample.database.windows.net/exdb?charset=utf8mb4"
+# mysql://username:password@host:port/database_name
 
-engine = create_engine(db_conn_str)
+url_object = URL.create(
+    "mysql+mysqlconnector",
+    username="admin",
+    password="admin",  # plain (unescaped) text
+    host="localhost",
+    port="3306",
+    database="exchangedb",
+)
+
+engine = create_engine(url_object)
+
+# Test the connection
+connection = engine.connect()
+
+result = connection.execute(text("SELECT * FROM user;"))
+for row in result:
+    print(row)
+# username = 'admin'
+# password = 'admin'
+# host = 'localhost'
+# port = '3306'
+# database_name = 'uni-pov-exchange-db'
+# #db_conn_str = "mysql+pymysql://exchange:Unipovex8@server8-sample.database.windows.net/exdb?charset=utf8mb4"
+# db_conn_str = 'mysql+pymysql://' + username + ':' + password + '@' + host + ':' + port + '/' + database_name
+
+
+# engine = create_engine(db_conn_str)
--- server/schema.sql
+++ server/schema.sql
@@ -1,28 +1,35 @@
-CREATE TABLE User
-(
-    userID INTEGER PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    nationality VARCHAR(3), 
-    homeUniversity INTEGER,
-    exchangeUniversity INTEGER,
-    UNIQUE(username),
-    FOREIGN KEY (homeUniversity) REFERENCES University(UniversityID)
-    FOREIGN KEY (exchangeUniversity) REFERENCES University(UniversityID)
-    FOREIGN KEY (nationality) REFERENCES Country(countryCode)
-);
+drop database if exists exchangeDB;
+create database exchangeDB;
+use exchangeDB;
 
 CREATE TABLE University (
     universityID INTEGER PRIMARY KEY,
-    countryCode VARCHAR(3),
+    -- countryCode VARCHAR(3), 
     longName VARCHAR(255),
-    infoPageID INTEGER,
-    FOREIGN KEY (infoPageID) REFERENCES InfoPage(infoPageID)
-    FOREIGN KEY (countryCode) REFERENCES Country(countryCode)
+    infoPageID TEXT
+    -- FOREIGN KEY (infoPageID) REFERENCES InfoPage(infoPageID), 
+    -- FOREIGN KEY (countryCode) REFERENCES Country(countryCode) 
 );
+
+CREATE TABLE User
+(
+    -- userID INTEGER PRIMARY KEY,
+    username VARCHAR(30) PRIMARY KEY,
+    -- nationality VARCHAR(3),
+    homeUniversity INTEGER,
+    -- exchangeUniversity INTEGER, 
+    UNIQUE(username),
+    FOREIGN KEY (homeUniversity) REFERENCES University(UniversityID)
+    -- FOREIGN KEY (exchangeUniversity) REFERENCES University(UniversityID), 
+    -- FOREIGN KEY (nationality) REFERENCES Country(countryCode)
+);
+
+
 
 -- Many-to-Many
 -- https://dba.stackexchange.com/questions/74627/difference-between-on-delete-cascade-on-update-cascade-in-mysql
 -- ^ ON DELETE CASCADE ON UPDATE CASCADE
+/*
 CREATE TABLE Favorites (
     user INTEGER NOT NULL,
     university INTEGER NOT NULL,
@@ -33,7 +40,7 @@ CREATE TABLE Favorites (
     CONSTRAINT university_fk_con
         FOREIGN KEY (university) REFERENCES University (universityID)
         ON DELETE CASCADE ON UPDATE CASCADE,
-)
+);
 
 CREATE TABLE InfoPage (
     infoPageID INTEGER AUTO_INCREMENT,
@@ -41,11 +48,23 @@ CREATE TABLE InfoPage (
     semesterText TEXT,
     applicationText TEXT,
     coursesText TEXT,
-    tuitionText TEXT,
+    tuitionText TEuniversityXT,
     housingText TEXT,
     visaText TEXT,
     PRIMARY KEY (infoPageID)
-)
+);
+*/
+insert into University(universityID, longName, infoPageID) values
+('1', 'NTNU', 'Trondheim is best'),
+('2', 'Uio', 'Oslo is best'),
+('3', 'Uib', 'Bergens is best');
+
+insert into User(username, homeUniversity) values
+('bob', '1'),
+('Ola', '2'),
+('Kari', '2');
+
+
 
 
 
