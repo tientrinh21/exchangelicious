fb5d7f68a5c7ee38b4974814aa6ff7517ee67293
tientrinh21 <tqtien.a11619@gmail.com>
2024-04-20 22:30:41 +0900

    style: Change logo, update title

--- client/components/site-logo.tsx
+++ client/components/site-logo.tsx
@@ -1,10 +1,12 @@
-import Link from "next/link"
+import Link from 'next/link'
 
 export function SiteLogo() {
   return (
     <Link href="/" className="flex items-center space-x-3">
-      <img src="https://github.com/shadcn.png" alt="eduventure" className="w-8 rounded-full"></img>
-      <span className="font-extrabold inline-block text-foreground text-lg md:text-xl">ExchanGe</span>
+      <img src="/logo.png" alt="EG" className="w-9 rounded-full"></img>
+      <span className="inline-block text-lg font-extrabold text-foreground md:text-xl">
+        ExchanGe
+      </span>
     </Link>
   )
 }
