9727e9dff14f33f45f4c519e230890d28720147c
rheem-hari <rheemhari@gmail.com>
2024-04-29 16:56:55 +0900

    refactor: removed unused codes

--- client/components/ex-page-uni-info.tsx
+++
@@ -1,9 +0,0 @@
-// exchange-page-uni-info.tsx
-
-import React from 'react';
-
-export function HeaderString({ className, children }: { className: string; children: React.ReactNode }) {
-  return (
-    <h1 className={className}>{children}</h1>
-  );
-}
