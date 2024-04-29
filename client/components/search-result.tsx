f1e9ac1b990f422962a7c1212fcddb6a042eaa3c
rheem-hari <rheemhari@gmail.com>
2024-04-29 16:35:26 +0900

    style: change muted color & remove unused code

--- client/components/search-result.tsx
+++ client/components/search-result.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link'; // Next.js의 Link 컴포넌트를 임포트합니다.
 
-function SearchResult() {
+export function SearchResult() {
   // 대학의 고유 코드 (나중에 DB에서 받아와 사용할 수 있음)
   const universityCode = 'skku'; // 현재는 'skku'로 고정되어 있으나, DB 연결 후 동적으로 변경 가능
 
@@ -12,18 +12,16 @@ function SearchResult() {
           {/* Left Section */}
           <div>
             <h2 className="text-2xl font-bold mb-1.5">Sungkyunkwan University</h2>
-            <p className="text-muted-foreground font-semibold">Seoul, South Korea | Suwon Campus | F2 Visa</p>
+            <p className="text-muted font-semibold">Seoul, South Korea | Suwon Campus | F2 Visa</p>
           </div>
 
           {/* Right Section */}
           <div className="text-right">
-            <p className="text-muted-foreground font-semibold">QS Ranking: 115</p>
-            <p className="text-muted-foreground font-semibold">Dormitory: Available</p>
+            <p className="text-muted font-semibold">QS Ranking: 115</p>
+            <p className="text-muted font-semibold">Dormitory: Available</p>
           </div>
         </div>
       </div>
     </Link>
   );
 }
-
-export default SearchResult;
