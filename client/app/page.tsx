239952e911fc9a3260d6fc537bc8a9111d6dfc30
rheem-hari <164372390+rheem-hari@users.noreply.github.com>
2024-05-02 13:51:14 +0900

    Update page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,8 +2,6 @@
 import React from 'react'
 import ConsumeApiDemo from '@/components/consumeApiDemo';
 import PaginationDemo from '@/components/paginationDemo';
-import React from 'react'
-import ConsumeApiDemo from '@/components/consumeApiDemo'
 
 export default function HomePage() {
   return (
