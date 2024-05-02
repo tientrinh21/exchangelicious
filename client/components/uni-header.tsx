7f523c886143fee35bd4637dc1e6eeac652ad4d8
tientrinh21 <tqtien.a11619@gmail.com>
2024-05-02 15:36:42 +0900

    feat: add fallback img for university with no header img

--- client/components/uni-header.tsx
+++ client/components/uni-header.tsx
@@ -29,11 +29,13 @@ function UniHeaderImgWrapper(props: {
   children: React.ReactNode
   imgSrc: string
 }) {
+  const fallbackImgSrc = '/fallback-uni.jpg'
+
   return (
     <div
       className="md:80 flex h-48 w-screen items-end justify-center bg-cover bg-center sm:h-72 lg:h-96"
       style={{
-        backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${props.imgSrc})`,
+        backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${props.imgSrc}), url(${fallbackImgSrc})`,
       }}
     >
       {props.children}
