788ad35cb497b2cd5748f6b03a8d80a927ce3350
Tien Trinh <57804513+tientrinh21@users.noreply.github.com>
2024-05-17 02:47:11 +0000

    refctor: rewrite raw_sql and update api doc

--- server/resources_api/universities.py
+++ server/resources_api/universities.py
@@ -4,24 +4,31 @@ from resources_api.resource_fields_definitions import (
     university_with_info_resource_fields,
     search_universities_resource_fields,
 )
-from sqlalchemy import select, text
+from sqlalchemy import select, text, join
 from database.database_setup import db
-from database.models import InfoPageTable, UniversityTable
+from database.models import InfoPageTable, UniversityTable, CountryTable
 
 
 class UniversityRes(Resource):
     @marshal_with(university_resource_fields)
     def get(self, university_id):
-        # TODO: Rewirte this
-        sql_raw = "SELECT * FROM university_table JOIN country_table ON university_table.country_code = country_table.country_code WHERE university_table.university_id = :val"
-        res = db.session.execute(text(sql_raw), {"val": university_id}).first()
-        print(res)
+        stmt = (
+            select(UniversityTable, CountryTable)
+            .join(CountryTable, UniversityTable.country_code == CountryTable.country_code)
+            .where(UniversityTable.university_id == university_id)
+        )
+        res = db.session.execute(stmt).first()
         if res is None:
             abort(
                 message=f"No university with the ID '{university_id}'.",
                 http_status_code=404,
             )
-        return res, 200
+
+        parent, child = res
+        result = parent.__dict__
+        result.update(child.__dict__)
+
+        return result, 200
 
 
 class UniversityWithInfoRes(Resource):
