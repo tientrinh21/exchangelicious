060cc9f4bffebad0cf5966155d8c4aa057356714
Tien Trinh <57804513+tientrinh21@users.noreply.github.com>
2024-05-20 15:24:29 +0000

    chore: fix logo's <img> tag

--- client/components/site-logo.tsx
+++ client/components/site-logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export function SiteLogo() {
   return (
     <Link href="/" className="flex items-center space-x-2 md:space-x-3">
-      <img src="/logo.png" alt="EG" className="h-12 w-12 rounded-full"></img>
+      <img src="/logo.png" alt="logo" className="h-12 w-12 rounded-full" />
       <span className="inline-block text-lg font-extrabold text-foreground md:text-xl">
         Exchangelicious
       </span>
