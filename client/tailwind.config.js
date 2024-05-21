c1960d4767a28976b51143b1e244af239d1f0ade
tientrinh21 <tqtien.a11619@gmail.com>
2024-05-21 22:47:15 +0900

    chore: change color system & clean unused files
    chore: clean unused files
    
    chore

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
+        placeholder: 'hsl(var(--placeholder))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         primary: {
@@ -74,9 +75,6 @@ module.exports = {
       fontSize: {
         'xl-plus': '1.375rem',
       },
-      placeholderColor: {
-        custom: '#B9C1CA' 
-      }
     },
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
