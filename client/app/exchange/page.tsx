a2751b4e4e05a7a5e09e6332fcd90aaa01741c38
tientrinh21 <tqtien.a11619@gmail.com>
2024-06-08 23:05:26 +0900

    style (/exchange): change spacing
    style: spacing

--- client/app/exchange/page.tsx
+++ client/app/exchange/page.tsx
@@ -73,36 +73,31 @@ export default function ExchangePage() {
   return (
     <>
       <div className="container mx-auto max-w-screen-xl px-4 sm:px-8">
-        <div className="py-10 text-center">
+        <div className="py-5 md:py-8 text-center">
           <h1 className="text-xl-plus font-bold text-secondary-foreground">
-            More than <span className="text-primary">500</span> universities are
+            More than <span className="text-primary">70</span> universities are
             connected
           </h1>
         </div>
 
         <div className="flex flex-col items-start justify-between lg:flex-row">
-          {/* <div className="mb-4 w-full rounded-lg border border-gray-200 bg-white p-4 shadow-md lg:mb-0 lg:w-1/4"> */}
-          {/*   <div className="text-center">Filter Container</div> */}
-          {/* </div> */}
-
-          {/* <div className="flex w-full flex-col p-4 lg:w-3/4"> */}
-          <div className="flex w-full flex-col p-4">
+          <div className="flex w-full flex-col">
             <div className="w-full">
               <form className="mx-auto max-w-full">
                 <label
                   htmlFor="default-search"
-                  className="sr-only mb-2 text-sm font-medium text-muted-foreground dark:text-white"
+                  className="sr-only"
                 >
                   Search
                 </label>
-                <div className="relative mb-5">
-                  <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
+                <div className="relative mb-5 mx-2">
+                  <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center px-2">
                     <MagnifyingGlassIcon className="h-5 w-5 text-placeholder" />
                   </div>
                   <input
                     type="search"
                     id="default-search"
-                    className="focus:border-b-3 block w-full rounded-t-lg border-b-2 border-secondary bg-background p-4 pl-14 text-lg font-medium text-secondary-foreground placeholder:text-placeholder focus:border-primary focus:outline-none focus:ring-0"
+                    className="focus:border-b-3 block w-full rounded-t-lg border-b-2 border-secondary bg-background p-2 pl-10 sm:pl-12 text-lg font-medium text-secondary-foreground placeholder:text-placeholder focus:border-primary focus:outline-none focus:ring-0"
                     placeholder="Search universities by name, region, country, etc :)"
                     value={query}
                     onChange={handleSearch}
@@ -130,7 +125,7 @@ export default function ExchangePage() {
                     </p>
                   }
                 >
-                  <div className="flex flex-col gap-3">
+                  <div className="flex flex-col gap-4">
                     {universities.map((university) => (
                       <UniCard
                         key={university.university_id}
