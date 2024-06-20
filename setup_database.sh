89ad1ddad5995e42f880fc2fa9906c8097b7349b
tientrinh21 <tqtien.a11619@gmail.com>
2024-06-20 21:50:43 +0900

    chore: remove 'deactive' on shell script

--- setup_database.sh
+++ setup_database.sh
@@ -4,39 +4,39 @@ ENV_FILE_PATH="server/.env"
 
 # Load environment variables from the .env file
 if [ -f "$ENV_FILE_PATH" ]; then
-  export $(cat $ENV_FILE_PATH | grep -v '^#' | xargs)
+	export $(cat $ENV_FILE_PATH | grep -v '^#' | xargs)
 else
-  echo ".env file not found!"
-  exit 1
+	echo ".env file not found!"
+	exit 1
 fi
 
 # Check if required environment variables are set
 if [ -z "$DB_USERNAME" ] || [ -z "$DB_PASSWORD" ]; then
-  echo "One or more required environment variables are missing!"
-  exit 1
+	echo "One or more required environment variables are missing!"
+	exit 1
 fi
 
 # Function to execute a SQL file
 execute_sql_file() {
-  local sql_file=$1
-  echo "Executing $sql_file..."
-  mysql -u$DB_USERNAME -p$DB_PASSWORD < $sql_file
+	local sql_file=$1
+	echo "Executing $sql_file..."
+	mysql -u$DB_USERNAME -p$DB_PASSWORD <$sql_file
 
-  if [ $? -eq 0 ]; then
-    echo "SQL script $sql_file executed successfully!"
-  else
-    echo "Error executing SQL script $sql_file!"
-    exit 1
-  fi
+	if [ $? -eq 0 ]; then
+		echo "SQL script $sql_file executed successfully!"
+	else
+		echo "Error executing SQL script $sql_file!"
+		exit 1
+	fi
 }
 
 # Run the setup - the paths may be different on a different computer
 
 UNAME=$(uname)
 
-if [[ "$UNAME" == "Linux" || "$UNAME" == "Darwin" ]] ; then
+if [[ "$UNAME" == "Linux" || "$UNAME" == "Darwin" ]]; then
 	source ./uni-data/.venv/bin/activate
-elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]]; then
 	source ./uni-data/.venv/Scripts/activate
 fi
 
@@ -44,5 +44,3 @@ execute_sql_file ./server/database_schemas/countries.sql
 python ./uni-data/ranking/rank.py
 execute_sql_file ./server/database_schemas/schema.sql
 python ./uni-data/parse_md_to_db.py
-
-deactivate
