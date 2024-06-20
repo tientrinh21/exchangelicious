340b215cc172c0a00c19c0814b53d87fab75d38d
tientrinh21 <tqtien.a11619@gmail.com>
2024-06-21 00:20:48 +0900

    style: update header mobile

--- client/components/site-logo.tsx
+++ client/components/site-logo.tsx
@@ -2,9 +2,13 @@ import Link from 'next/link'
 
 export function SiteLogo() {
   return (
-    <Link href="/" className="flex items-center space-x-2 md:space-x-3">
-      <img src="/logo.png" alt="logo" className="h-12 w-12 rounded-full" />
-      <span className="inline-block text-lg font-extrabold text-foreground md:text-xl">
+    <Link href="/" className="flex items-center sm:space-x-2 md:space-x-3">
+      <img
+        src="/logo.png"
+        alt="logo"
+        className="h-9 w-9 rounded-full sm:h-12 sm:w-12"
+      />
+      <span className="inline-block text-base font-extrabold text-foreground sm:text-lg md:text-xl">
         Exchangelicious
       </span>
     </Link>
